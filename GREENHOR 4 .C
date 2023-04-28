ececea052e5b8df8b89aefc313c030a646fb26b2
harsh savani <131761988+harshsavani69@users.noreply.github.com>
2023-04-28 15:35:10 +0530

    Add files via upload

---
+++ GREENHOR 4 .C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+   clrscr();
+   int a,b;
+   printf("enter value a=");
+   scanf("%d",&b);
+   printf("enter value b=");
+   scanf("%d",&a);
+   printf("additon of a=%d",a);
+   printf("additon of b=%d",b);
+
+
+   getch();
+}
